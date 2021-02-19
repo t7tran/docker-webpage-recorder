085c221b76cf666efb27ad8ccfaa5061888a8fb3
Tien Tran <tien@transfamily.info>
2021-02-19 16:42:19 +1100

    Improve progress logging and termination logic

--- rootfs/recording/run.sh
+++ rootfs/recording/run.sh
@@ -10,47 +10,57 @@ fi
 SIGNALS='SIGHUP SIGINT SIGQUIT SIGTERM'
 _terminating() {
 	trap - $SIGNALS 
+	set +e
 
 	if [[ -n "$FF_PID" ]]; then
-		sleep ${FF_SHUTDOWN_GRACE:-3} &
-		SLEEP_PID=$!
 		echo Shutdown Firefox $FF_PID
-		kill $FF_PID
+		#kill -TERM $FF_PID &>/dev/null
+		for pid in `pidof firefox`; do kill -TERM $pid; done
+		sleep ${FF_SHUTDOWN_GRACE:-3}
 	fi
-	[[ -n "$XVFB_PID" ]] && kill $XVFB_PID
+	[[ -n "$XVFB_PID" ]] && kill $XVFB_PID &>/dev/null
 	pulseaudio --kill &>/dev/null
 
-	wait $SLEEP_PID
 	echo Terminated
 }
 trap _terminating $SIGNALS
 
-set -xeo pipefail
+set -eo pipefail
 
 SCREEN_WIDTH=${RECORDING_SCREEN_WIDTH:-'1280'}
 SCREEN_HEIGHT=${RECORDING_SCREEN_HEIGHT:-'720'}
 SCREEN_RESOLUTION=${SCREEN_WIDTH}x${SCREEN_HEIGHT}
-COLOR_DEPTH=24
+COLOR_DEPTH=${COLOR_DEPTH:-24}
 X_SERVER_NUM=1
-S3_BUCKET_NAME=${RECORDING_ARTIFACTS_BUCKET}
+S3_BUCKET_NAME=${RECORDING_ARTIFACTS_BUCKET:?}
+
+echo "Recorder started with resolution $SCREEN_RESOLUTION (${COLOR_DEPTH}-bit)"
+echo "Storing artefacts to S3 bucket '${S3_BUCKET_NAME}'"
 
 # Start PulseAudio server so Firefox will have somewhere to which to send audio
+echo Starting PulseAudio server
 pulseaudio -D --exit-idle-time=-1
 pacmd load-module module-virtual-sink sink_name=v1  # Load a virtual sink as `v1`
 pacmd set-default-sink v1  # Set the `v1` as the default sink device
 pacmd set-default-source v1.monitor  # Set the monitor of the v1 sink to be the default source
+echo Started PulseAudio server
 
 # Start X11 virtual framebuffer so Firefox will have somewhere to draw
+echo Starting X11
 Xvfb :${X_SERVER_NUM} -ac -screen 0 ${SCREEN_RESOLUTION}x${COLOR_DEPTH} > /dev/null 2>&1 &
 XVFB_PID=$!
 export DISPLAY=:${X_SERVER_NUM}.0
 sleep 0.5  # Ensure this has started before moving on
+echo Started X11
 
 # Create a new Firefox profile for capturing preferences for this
+echo Preparing Firefox profile
 firefox --no-remote --new-instance --createprofile "foo4 /opt/firefox"
+echo Prepared Firefox profile
 
 # Set the Firefox preferences to enable automatic media playing with no user
 # interaction and the use of the OpenH264 plugin.
+echo Preparing Firefox preferences
 cat <<EOF >> /opt/firefox/prefs.js
 user_pref("media.autoplay.default", 0);
 user_pref("media.autoplay.enabled.user-gestures-needed", false);
@@ -66,6 +76,7 @@ user_pref("media.getusermedia.insecure.enabled", true);
 user_pref("devtools.toolbox.selectedTool", "${FF_DEVTOOLS_TAB:-webconsole}");
 user_pref("browser.sessionstore.interval", ${FF_SESSIONSTORE_INTERVAL:-15000});
 EOF
+echo Prepared Firefox preferences
 
 # Start Firefox browser and point it at the URL we want to capture
 #
@@ -73,6 +84,7 @@ EOF
 # argument list or else only a white screen will result in the capture for some
 # reason.
 
+echo Starting Firefox with URL ${RECORDING_URL}
 firefox \
   -P foo4 \
   --width ${SCREEN_WIDTH} \
@@ -84,17 +96,13 @@ firefox \
   ${FF_DEVTOOLS_VISIBLE:+--devtools} \
   ${FF_JSCONSOLE_VISIBLE:+--jsconsole} \
   --ssb ${RECORDING_URL} \
+  >/dev/null \
   &
 
 FF_PID=$!
-echo Firefox PID $FF_PID
+echo Started Firefox with PID $FF_PID
 sleep 0.5  # Ensure this has started before moving on
 xdotool mousemove 1 1 click 1  # Move mouse out of the way so it doesn't trigger the "pause" overlay on the video tile
-
-[[ -z "$START_HASH" && -z "$STOP_HASH" && -z "$EXIT_HASH" ]] &&  
-	exec node /recording/record.js ${S3_BUCKET_NAME} ${SCREEN_WIDTH} ${SCREEN_HEIGHT}
-
-set +x
 
 REC_PID=
 SHUTTINGDOWN=
@@ -114,6 +122,7 @@ stop() {
 }
 _shutdown() {
 	trap - $SIGNALS
+	set +e
 
 	echo Shutting down
 	SHUTTINGDOWN=yes
@@ -123,6 +132,12 @@ _shutdown() {
 }
 
 trap _shutdown $SIGNALS
+
+if [[ -z "$START_HASH" && -z "$STOP_HASH" && -z "$EXIT_HASH" ]]; then
+	start
+	wait
+	exit 0
+fi
 
 SESSION_FILE=/opt/firefox/sessionstore-backups/recovery.jsonlz4
 for (( i=1; i<=60; i++ )); do [[ -f $SESSION_FILE || -n "$SHUTTINGDOWN" ]] && break || sleep 1; done
--- rootfs/recording/utils/upload.js
+++ rootfs/recording/utils/upload.js
@@ -17,20 +17,20 @@ class S3Uploader {
         this.bucket = bucket;
         this.key = key;
         this.s3Uploader = new AWS.S3({ params: { Bucket: bucket, Key: key } });
-        console.log(`[upload process] constructed a S3 object with bucket: ${this.bucket}, key: ${this.key}`);
+        console.log(`[upload process] constructed an S3 object at: s3://${this.bucket}/${this.key}`);
     }
 
     uploadStream(stream, tags) {
         const managedUpload = this.s3Uploader.upload({ Body: stream }, { tags }, (err, data) => {
             if (err) {
-                console.log('[stream upload process] - failure - error handling on failure', err);
+                console.error('[stream upload process] failure - error handling on failure', err);
             } else {
-                console.log(`[stream upload process] - success - uploaded the file to: ${data.Location}`);
+                console.log(`[stream upload process] success - uploaded the file to: ${data.Location}`);
                 process.exit();
             }
         });
         managedUpload.on('httpUploadProgress', function (event) {
-            console.log(`[stream upload process]: on httpUploadProgress ${event.loaded} bytes`);
+            console.log(`[stream upload process] uploaded ${event.loaded} bytes`);
         });
     }
 }
